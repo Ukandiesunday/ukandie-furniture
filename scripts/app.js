102b1d65e2d02168f9991a55e06f24b4088f19b4
ukandieSunday <ukandieudo202@gmail.com>
2023-10-08 04:05:17 +0100

    generate category buttons

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,25 @@
+import { products } from "./products.js";
+
 // To toggle the menu icon
 const menuToggle = document.querySelector(".menu-toggle");
 const sideBar = document.querySelector(".nav-ul");
 menuToggle.addEventListener("click", (event)=>{
  menuToggle.classList.toggle("toggle");
  sideBar.classList.toggle("toggle-sidebar")
-})
+});
 
+const buttons = document.querySelector(".buttons");
+const  showCategory = ()=> {
+ const categories = products.reduce((values, item)=>{
+if(!values.includes(item.category)){
+  values.push(item.category)
+}
+return values
+  },["All"])
+
+  categories.map((category)=>{
+   buttons.innerHTML += `<button class="btns" data-category = ${category}>${category}</button>`
+  })
+}
+
+showCategory()
