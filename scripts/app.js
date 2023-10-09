1cce5499ce18ffe0cdb4895582a126c9353852d1
UkandieSunday <ukandieudo202@gmail.com>
2023-10-09 09:11:14 +0100

    add filtered menu catelogue

--- scripts/app.js
+++ scripts/app.js
@@ -3,23 +3,60 @@ import { products } from "./products.js";
 // To toggle the menu icon
 const menuToggle = document.querySelector(".menu-toggle");
 const sideBar = document.querySelector(".nav-ul");
-menuToggle.addEventListener("click", (event)=>{
+menuToggle.addEventListener("click", ()=>{
  menuToggle.classList.toggle("toggle");
  sideBar.classList.toggle("toggle-sidebar")
 });
 
-const buttons = document.querySelector(".buttons");
-const  showCategory = ()=> {
- const categories = products.reduce((values, item)=>{
-if(!values.includes(item.category)){
-  values.push(item.category)
-}
-return values
-  },["All"])
+window.addEventListener("DOMContentLoaded",()=>{
+  displayMenu(products);
+  displayMenuBtns();
+});
 
-  categories.map((category)=>{
-   buttons.innerHTML += `<button class="btns" data-category = ${category}>${category}</button>`
-  })
+//To generate category buttons
+function displayMenuBtns (){
+  const buttons = document.querySelector(".btns-container");
+  const categories = products.reduce((values, item)=>{
+ if(!values.includes(item.category)){
+   values.push(item.category)
+ }
+ return values
+   },["All"])
+ 
+   categories.map((category)=>{
+    buttons.innerHTML += `<button class="btns" data-category = ${category}>${category}</button>`
+   })
+ 
+   const filteredbtn = document.querySelectorAll(".btns");
+   filteredbtn.forEach((btn)=>{
+     btn.addEventListener("click",(event)=>{
+      const category = event.target.dataset.category;
+      const filteredProducts = products.filter((item)=>
+       item.category === category
+      );
+      if(category === "All"){
+       displayMenu(products)
+      }else{
+       displayMenu(filteredProducts)
+      }
+     });
+   });
 }
 
-showCategory()
+ const menu = document.querySelector(".menu");
+ function displayMenu (items){
+   menu.innerHTML = items.map((item)=>{
+     return `<div class="menu-cols">
+     <div class="img-container">
+       <img src="${item.img}" >
+     </div>
+     <p class="name">${item.name}</p>
+     <p class="price">${item.price}</p>
+     <p class="desc">${item.desc}</p>
+     <p class="read">Read More</p>
+     </div>
+     `
+   }).join("");
+ }
+
+ const bgColor = docuemn
